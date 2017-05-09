f62fb3c234fe0c04c00a120a6700760e09415c30
mahr <mahaoran0411@gmail.com>
2017-05-09 13:54:23 +0800

    modify button and welcome_controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    flash[:notice] = "Good Morning！"
+    flash[:alert] = "Good Evening,go to sleep,come on.."
+    flash[:warning] = "this is warning!"
   end
 end
